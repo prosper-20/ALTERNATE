1d0e6dfcbeabcfdc086c73a2e0868b04a0ff4b9b
prosper-20 <edwardprosper001@gmail.com>
2022-04-29 11:32:42 +0100

    updates

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Category
 # Register your models here.
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ["title", "author", "date_posted"]
 
 admin.site.register(Post, PostAdmin)
+admin.site.register(Category)
--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from distutils.command.upload import upload
+from tabnanny import verbose
 from time import time
 from django.db import models
 from django.utils import timezone
@@ -9,7 +10,10 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
-    def _str__(self):
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
         return self.name
 
 
@@ -20,6 +24,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to="post_images")
     slug = models.SlugField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
 
     def __str__(self):
