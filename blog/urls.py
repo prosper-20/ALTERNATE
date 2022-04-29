c1d030a902026e924f882828147788feaa887754
prosper-20 <edwardprosper001@gmail.com>
2022-04-29 12:40:58 +0100

    updates

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView
 
 
 urlpatterns = [
 
     path("home/", PostListView.as_view(), name="blog_home"),
     path('post/<str:slug>/', PostDetailView.as_view(), name="post_detail"),
-    path('post/new/', PostCreateView.as_view(), name="post_create")
+    path('post/new/', PostCreateView.as_view(), name="post_create"),
+    path('post/<str:slug>/update/', PostUpdateView.as_view(), name="post_update")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from pyexpat import model
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 # Create your views here.
 
 
@@ -25,12 +26,50 @@ class PostDetailView(DetailView):
     context_object_name = "post"
 
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'content', 'image', 'slug', 'category']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content', 'image', 'slug', 'category']
+
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    fields = ['title', 'content', 'image', 'slug', 'category']
+    success_url = "/"
+
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+
+
+
 
 
