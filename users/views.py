478d74adaf5f831fa530ae1ede8dec0747f437ad
prosper-20 <edwardprosper001@gmail.com>
2022-04-30 20:44:00 +0100

    updates

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages, auth
 from django.contrib.auth.models import User
+from .forms import UserCreationForm, UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 # Create your views here.
 
