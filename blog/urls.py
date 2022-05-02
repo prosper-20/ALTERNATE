504961570ab9bb2cd68d262463d240b5c1b6fce1
prosper-20 <edwardprosper001@gmail.com>
2022-05-02 22:58:53 +0100

    updates

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView,
 urlpatterns = [
 
     path("home/", PostListView.as_view(), name="blog_home"),
-    path('post/<str:slug>/', PostDetailView.as_view(), name="post_detail"),
+    path('post/<str:slug>/', PostDetailView, name="post_detail"),
     path('post/new/', PostCreateView.as_view(), name="post_create"),
     path('post/<str:slug>/update/', PostUpdateView.as_view(), name="post_update"),
     path('post/<str:slug>/delete/', PostDeleteView.as_view(), name="post_delete"),
