766eca2e9621fadfaada5f0bc5c44880e064c3a7
prosper-20 <edwardprosper001@gmail.com>
2022-05-02 23:16:32 +0100

    updates

--- blog/models.py
+++ blog/models.py
@@ -37,7 +37,7 @@ class Post(models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
-    name = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
     body = models.TextField('Enter your commment...')
     date_added = models.DateTimeField(auto_now_add=True)
     # This was added from the web
