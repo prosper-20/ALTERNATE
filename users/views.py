ebb96e649f0cf4fe6ac6c1957c904c07b5ce3dd1
prosper-20 <edwardprosper001@gmail.com>
2022-05-23 22:14:09 +0100

    updates

--- users/views.py
+++ users/views.py
@@ -28,8 +28,8 @@ def register(request):
                 return redirect("home")
         else:
             messages.info(request, "Both passowrds do not match")
-            # You just it from register.html to regsiter_1
-    return render(request, 'users/register_1.html')
+            # You just it from register.html to regsiter_1 to register_2
+    return render(request, 'users/register_2.html')
 
 def login(request):
     if request.method == "POST":
